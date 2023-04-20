4865edfc493c572958e9b8ab9d1dcd0516762ab9
Mario Martinez <mario3alberto3@gmail.com>
2023-04-20 13:01:25 -0500

    added MenuView class

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework import generics
 
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
+
+class MenuView(generics.ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
