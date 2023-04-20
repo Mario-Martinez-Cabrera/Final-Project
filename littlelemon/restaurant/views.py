8da72bdfaea425117d18e4e1af6ad523cc0dd629
Mario Martinez <mario3alberto3@gmail.com>
2023-04-20 13:07:19 -0500

    added SingleMenuItemView class for API view

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -12,3 +12,7 @@ def home(request):
 class MenuView(generics.ListCreateAPIView):
   queryset = Menu.objects.all()
   serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
