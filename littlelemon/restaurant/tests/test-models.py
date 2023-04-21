132f6231462593fb393a61aa63165f5d1c392636
Mario Martinez <mario3alberto3@gmail.com>
2023-04-21 14:57:39 -0500

    moved files

--- littlelemon/restaurant/tests/test-models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from models import Menu
-
-class MenuTest(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(title="Ice Cream", price=80, inventory=100)
-        self.assertEqual(item, "Ice Cream. Price at 80. Total inventory is 100")
